1284437327b64ee9f771fdc645b2058f92b02998
Latasha Simmons <lsimmons1832@gmail.com>
2017-02-19 19:07:28 -0600

    assignment complete

--- songs.js
+++ songs.js
@@ -7,15 +7,37 @@ songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album App
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
 //Each student must add one song to the beginning.
-
-// for (var i = 0; i < songs.length; i++) {
-// 	songs.push('I Need You More');
-// }
-console.log(songs);
+	songs.unshift("Fill Me Up");
+//console.log("1",songs);
 //Each student must add one song to the end of the array.
-
+	songs.push("I Need You More");
+//console.log("2",songs);
 //Loop over the array and remove any words or characters that obviously don't belong.
 
+var newSong = [];
+for (var i = 0; i < songs.length; i++) {
+	newSong[i] = songs[i].replace("*", '').replace("!", '').replace("@", '').replace("(", '');
+}
+songs = newSong;
+console.log("3",newSong);
+console.log("3",songs);
 //Students must find and replace the > character in each item with a - character.
 
+for (var i = 0; i < newSong.length; i++) {
+	newSong[i] = songs[i].replace(">", "-");
+}
+console.log("4",newSong);
 //Add each string to the DOM in index.html in the main content area.
+function buildDOM() {
+var newSongElement = document.getElementById("songs");
+
+var list = newSongElement.innerHTML;
+list += "<ul>";
+for (var i = 0; i < newSong.length; i++) {
+		list += "<li>" + newSong[i] +"</li>";
+	
+	}
+list += "</ul>";
+newSongElement.innerHTML = list;
+}
+buildDOM();
