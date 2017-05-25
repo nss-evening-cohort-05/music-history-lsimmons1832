0b5073a2bf7a93335ecc5e488d273b7518e68bee
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-25 18:40:07 -0500

    assignment complete

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,67 +1,40 @@
-var musicHolder = document.getElementById('songs');
-var musicString = "";
-
-function writeToDOM(data){
-	for (var i = 0; i < data.songs.length; i++) {
-		console.log("I'm here");
-		var currentSong = data.songs[i];
-		 console.log(currentSong.Song);
-		 musicString += `<div><h1> ${data.songs[i].Song} </h1>`;
-		 musicString += `<p>${data.songs[i].Artist}  |  ${data.songs[i].Albumn}  |  ${data.songs[i].Genre}</p>  <button name="delete" class="delete" id="delBtn">DELETE</button>`;
-		 musicString += `</div>`;
-	}
-	musicHolder.innerHTML += musicString;
-	pageLoaded();
-}
-
-	window.addEventListener("click", function(event){
-	var delBtn = document.getElementById('delBtn');
-		console.log(event.target.parentNode);
-		console.log("I clicked delete");
-		if (event.target.id === "delBtn") {
-	  	event.target.parentNode.remove();
-			}
+$(document).ready(function() {
+	$.getJSON('./db/music.json', function(data){
+		$.forIn(data, function(index, value){
+			//console.log(value[0].Song);
+			console.log(index);
+			console.log(value);
+			// $.each(value);
+			$('.right-screen').append(`<div><h1>${value[0].Song}</h1><p>${value[0].Artist}  |  ${value[0].Albumn}  |  ${value[0].Genre}</p>  <button name="delete" class="delete" id="delBtn">DELETE</button></div>`);
+			$('.right-screen').append(`<div><h1>${value[1].Song}</h1><p>${value[1].Artist}  |  ${value[1].Albumn}  |  ${value[2].Genre}</p>  <button name="delete" class="delete" id="delBtn">DELETE</button></div>`);
+			$('.right-screen').append(`<div><h1>${value[2].Song}</h1><p>${value[2].Artist}  |  ${value[2].Albumn}  |  ${value[1].Genre}</p>  <button name="delete" class="delete" id="delBtn">DELETE</button></div>`);
+			$('.right-screen').append(`<div><h1>${value[3].Song}</h1><p>${value[3].Artist}  |  ${value[3].Albumn}  |  ${value[3].Genre}</p>  <button name="delete" class="delete" id="delBtn">DELETE</button></div>`);
+		//songs.push();
 	});
 	
-function pageLoaded(){
-	musicHolder.innerHTML += `<a id="more" href="#">More...</a>`;
-	var moreBtn = document.getElementById('more');
+});
 
 
-	moreBtn.addEventListener("click", function(event){
-		musicHolder.innerHTML = "";
-		var secondRequest = new XMLHttpRequest();
-		secondRequest.addEventListener("load", loadSecondFile);
-		secondRequest.addEventListener("error", loadSecondFileFailed);
-		secondRequest.open("GET", "./db/music2.json");
-		secondRequest.send();
-	});
 
-}
+  //   function writeToDOM() {
+  //   	$.each(music)
+  //     $('right-screen').append(`<div><h1>${music.Song}</h1></div>`);
+  //   }
 
-function loadFile(){
-	var data = JSON.parse(this.responseText);
-	writeToDOM(data);
-}
+  //   //load objects from json
+  //   $.ajax('./db/music.json').done(function(data) {
+  //       music = data;
+  //       $.ajax('./db/music2.json').done(function(data2) {
+  //           data2.forEach(function(song) {
+  //               music.push(song);
+  //           })
+  //           writeToDOM();
+  //       }).fail(function(error) {
+  //           console.log("There was a problem loading the second file");
+  //      })
+  //   	}).fail(function(error) {
+  //       console.log("There was a problem loading the file");
+  // });
 
-function loadFailed(){
-	alert("Sorry, There is a problem loading the file");
-}
 
-var myRequest = new XMLHttpRequest();
-myRequest.addEventListener("load", loadFile);
-myRequest.addEventListener("error", loadFailed);
-myRequest.open("GET", "./db/music.json");
-myRequest.send();
-
-function loadSecondFile(){
-	var data2 = JSON.parse(this.responseText);
-	writeToDOM(data2);
-	var moreBtn = document.getElementById('more');
-	moreBtn.disabled();
-}
-
-function loadSecondFileFailed(){
-	alert("Sorry, There is a problem loading the file");
-}
-
+});
