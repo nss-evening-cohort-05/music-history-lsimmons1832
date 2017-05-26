8fc0156ebbecab46dc6f039d6cad8998244712c0
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-25 20:37:08 -0500

    save progress

---
+++ javascripts/AppConstants.js
@@ -0,0 +1,8 @@
+ app.constant("FIREBASE_CONFIG", {
+    apiKey: "AIzaSyC0ueoblAdgUr5_cjVdy2Zmuo6X-9c8M3k",
+    authDomain: "musichistory-b81b8.firebaseapp.com",
+    databaseURL: "https://musichistory-b81b8.firebaseio.com",
+    projectId: "musichistory-b81b8",
+    storageBucket: "musichistory-b81b8.appspot.com",
+    messagingSenderId: "981235983469"
+  });
